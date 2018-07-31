c48e3a522478e4fd0f7fc9df6183f0971e992e48
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-31 23:40:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,8 @@ class Song
 
   def self.new_from_filename(file_name)
     split_name = file_name.split(/\s-|.mp3/)
-    self.new_by_name(split_name[1])
+    song_object = self.new_by_name(split_name[1])
+    song_object.artist_name=(split_name[0])
     binding.pry
   end
 end
